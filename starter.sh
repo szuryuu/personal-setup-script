48b7b66bc4038870755d113d904903002192e3f6
szuryuu <ilhamdzaky2007@gmail.com>
2025-11-30 21:09:47 +0700

    refactor: organize

--- starter.sh
+++ starter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make the script executable
-chmod +x folder-setup.sh
-chmod +x system-setup.sh
+chmod +x scripts/folder-setup.sh
+chmod +x scripts/system-setup.sh
 
-./folder-setup.sh
-./system-setup.sh
+./scripts/folder-setup.sh
+./scripts/system-setup.sh
